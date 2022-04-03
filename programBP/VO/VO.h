af83c1926a377f937c0672ac83352a48e7948e02
Jakub Hlaváček (221049) <78855484+Jakubhl@users.noreply.github.com>
2022-04-03 19:21:38 +0200

    Add files via upload

---
+++ programBP/VO/VO.h
@@ -0,0 +1,31 @@
+#ifndef VO_h
+#define VO_h
+#include "Arduino.h"
+
+class VO{
+    private:
+
+        byte a;
+        byte W1;
+        byte W2;
+        byte W3;
+        byte foto;
+        int foto_det;
+        int foto_sensor;
+        int analog_foto;
+        int convince;
+        unsigned long foto_t1;
+        unsigned long foto_t2;
+        unsigned long foto_t3;
+        unsigned long foto_t4;
+
+
+    public:
+
+        VO(byte led_W1, byte led_W2, byte led_W3, char analog_pin_fotoresistor, int foto_detect, int convince_time);
+	    void PinSetup();
+        void Sequence();
+        
+};
+#endif  
+
